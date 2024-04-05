73f327429f79a89b82727dfbfa1a525002b4905b
JeremyT1205 <jmt6561@psu.edu>
2024-04-04 21:14:07 -0400

    Outlined the methods for the game

--- src/main/java/org/example/View/Home_View.java
+++ src/main/java/org/example/View/Home_View.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.example.Controller.Home_Controller;
@@ -22,8 +23,22 @@ public class Home_View {
         Label selectLabel = new Label("Select a Game!");
         selectLabel.setStyle("-fx-font-size: 80px;");
 
-        String game1ImagePath = "file:///C:/Users/Jtuch/OneDrive/Desktop/SeniorProjectGUI/400x400.png";
-        String game2ImagePath = "https://source.unsplash.com/400x400/?Soccer";
+        Label game1Label = new Label("Game 1");
+        game1Label.setStyle("-fx-font-size: 40px;");
+
+        Label shootnscoreLabel = new Label("Shoot 'n' Score!");
+        shootnscoreLabel.setStyle("-fx-font-size: 40px;");
+
+        Label game2Label = new Label("Game 2");
+        game2Label.setStyle("-fx-font-size: 40px;");
+
+        Label simonSaysLabel = new Label("Simon Says!");
+        simonSaysLabel.setStyle("-fx-font-size: 40px;");
+
+
+        // Paths for images
+        String game1ImagePath = "file:///C:/Users/Jtuch/OneDrive/Desktop/SeniorProjectGUI/goaltempgrid400.jpg";
+        String game2ImagePath = "file:///C:/Users/Jtuch/OneDrive/Desktop/SeniorProjectGUI/goaltempsimon400.jpg";
 
         Button game1Button = createButton(game1ImagePath, "Game 1", "Shoot 'n' Score");
         Button game2Button = createButton(game2ImagePath, "Game 2", "Simon Says");
@@ -33,13 +48,28 @@ public class Home_View {
         enterButton.setStyle("-fx-font-size: 40px;");
         enterButton.setOnAction(e -> controller.openGameSettingWindow(primaryStage));
 
+        //Game1layout
+        VBox game1layout = new VBox(20);
+        game1layout.getChildren().addAll(game1Label, game1Button, shootnscoreLabel);
+        game1layout.setAlignment(Pos.CENTER);
+
+        //Game2layout
+        VBox game2layout = new VBox(20);
+        game2layout.getChildren().addAll(game2Label, game2Button, simonSaysLabel);
+        game2layout.setAlignment(Pos.CENTER);
+
+        //Gameslayout
+        HBox games = new HBox(20);
+        games.getChildren().addAll(game1layout, game2layout);
+        games.setAlignment(Pos.CENTER);
+
         // Layout
         VBox layout = new VBox(20);
-        layout.getChildren().addAll(selectLabel, game1Button, game2Button, enterButton);
+        layout.getChildren().addAll(selectLabel, games, enterButton);
         layout.setAlignment(Pos.CENTER);
 
         // Scene
-        Scene scene = new Scene(layout, 800, 800);
+        Scene scene = new Scene(layout, 1000, 900);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
