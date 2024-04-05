5dc51412fe093aa15bee76c9a370ef2a6f394a94
JeremyT1205 <jmt6561@psu.edu>
2024-04-04 22:04:57 -0400

    Cole you will need this update since you went offline prior to these changes. Home screen and Settings are  complete until we get final images for home screen buttons and when we implement a function to open selected game window after clicking continue in settings .

--- src/main/java/org/example/View/GameSetting_View.java
+++ src/main/java/org/example/View/GameSetting_View.java
@@ -15,6 +15,10 @@ public class GameSetting_View {
 
     public GameSetting_View(Stage primaryStage, String selectedGame) {
         this.controller = new GameSetting_Controller(selectedGame);
+        HBox playersHBox = controller.getPlayersHBox();
+        playersHBox.setAlignment(Pos.CENTER);
+
+        controller.updatePlayerCircles(controller.getTotalPlayers(), controller.getPlayersHBox());
         Stage settingsStage = new Stage();
         settingsStage.setTitle("Settings");
 
@@ -31,9 +35,6 @@ public class GameSetting_View {
         playersLabel.setStyle("-fx-font-size: 30px;");
         VBox spaceBeforePlayers = new VBox(20);
         spaceBeforePlayers.getChildren().add(new Label());
-
-        HBox playersHBox = new HBox(10);
-        playersHBox.setAlignment(Pos.CENTER);
 
         Button addPlayerButton = new Button("Add Player");
         addPlayerButton.setStyle("-fx-font-size: 20px;");
@@ -65,28 +66,42 @@ public class GameSetting_View {
         rounds10Button.setStyle("-fx-font-size: 20px;");
         rounds10Button.setOnAction(e -> controller.setRounds(10));
 
-        Button roundsInfinityButton = new Button("Infinity");
-        roundsInfinityButton.setStyle("-fx-font-size: 20px;");
-        roundsInfinityButton.setOnAction(e -> controller.setRounds(Integer.MAX_VALUE));
+        Button maxRoundsButton = new Button();
 
-        roundsButtonsBox.getChildren().addAll(rounds5Button, rounds10Button, roundsInfinityButton);
+        if(selectedGame.equals("Game 1")) {
+            // Set properties specific to "Game 1"
+            maxRoundsButton.setText("15");
+            maxRoundsButton.setStyle("-fx-font-size: 20px;");
+            maxRoundsButton.setOnAction(e -> controller.setRounds(15));
+        }
+        else {
+            // Set properties for other cases
+            maxRoundsButton.setText("Infinity");
+            maxRoundsButton.setStyle("-fx-font-size: 20px;");
+            maxRoundsButton.setOnAction(e -> controller.setRounds(Integer.MAX_VALUE));
+        }
 
-        Label speedLabel = new Label("Does speed affect points?");
+
+
+// Now, maxRoundsButton is accessible here
+        roundsButtonsBox.getChildren().addAll(rounds5Button, rounds10Button, maxRoundsButton);
+
+        Label speedLabel = new Label("Select your difficulty:");
         speedLabel.setStyle("-fx-font-size: 30px;");
         VBox spaceBeforeSpeed = new VBox(20);
         spaceBeforeSpeed.getChildren().add(new Label());
 
-        Button yesButton = new Button("Yes");
-        yesButton.setStyle("-fx-font-size: 20px;");
-        yesButton.setOnAction(e -> controller.setSpeedMode(true));
+        Button easyButton = new Button("Easy");
+        easyButton.setStyle("-fx-font-size: 20px;");
+        easyButton.setOnAction(e -> controller.setSpeedMode(false));
 
-        Button noButton = new Button("No");
-        noButton.setStyle("-fx-font-size: 20px;");
-        noButton.setOnAction(e -> controller.setSpeedMode(false));
+        Button hardButton = new Button("Hard");
+        hardButton.setStyle("-fx-font-size: 20px;");
+        hardButton.setOnAction(e -> controller.setSpeedMode(true));
 
         HBox speedModeButtons = new HBox(20);
         speedModeButtons.setAlignment(Pos.CENTER);
-        speedModeButtons.getChildren().addAll(yesButton, noButton);
+        speedModeButtons.getChildren().addAll(easyButton, hardButton);
 
         VBox spaceAfterContinue = new VBox(20);
         spaceAfterContinue.getChildren().add(new Label());
